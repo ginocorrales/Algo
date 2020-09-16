b68ae599c8ceeef25162a28083d5af8d72d9a565
Gino Corrales <gino.p.corrales@gmail.com>
2020-09-16 15:56:00 -0500

    adding couple more examples
    adding couple more examples

--- 5thAssign/DoubleList.h
+++ 5thAssign/DoubleList.h
@@ -4,8 +4,8 @@
 Author: Gino P. Corrales-Delgado
 IT 279 - Assign 5
 
-This is the header of the DoubleList Class. This class manages  dynamic array of doubles, also includes several functions to 
-test the class, such as adding a double, remove it, sending the index and returning the value of the element, and returning 
+This is the header of the DoubleList Class. This class manages  dynamic array of doubles, also includes several functions to
+test the class, such as adding a double, remove it, sending the index and returning the value of the element, and returning
 the number of doubles that actually in the list
 */
 #include <iostream>
@@ -14,14 +14,14 @@ using namespace std;
 class DoubleList{
 
   public:
-    
+
 	  DoubleList();  													 // constructor of the class
-    DoubleList(const DoubleList& dlObject);  // copy constructor
+    DoubleList(const DoubleList& dlObject);  // constructor overload
 		~DoubleList();													 // destructor of the class
-    
+
 		void addDouble(double n);  	 // adds a double to the end of the list
 		void removeDouble(); 				 // remove the last double in the list
-		double getDouble(int index); // accept an int parameter and returns the value of the double at that index   
+		double getDouble(int index); // accept an int parameter and returns the value of the double at that index
 		double getNumDoubles(); 		 // returns the number of doubles actually in the list
 
 		DoubleList& operator=(const DoubleList& rightSide);  // overload assign operator
@@ -38,4 +38,3 @@ class DoubleList{
 };
 
 #endif
-
--- 5thAssign/DoubleListTest.cpp
+++ 5thAssign/DoubleListTest.cpp
@@ -2,7 +2,7 @@
 Author: Gino P. Corrales-Delgado
 IT 279 - Assign 5
 
-This file will test the DoubleList Class, caltesting its functions even the assign operator. 
+This file will test the DoubleList Class, caltesting its functions even the assign operator.
 I am attaching also the Makefile to help comple and run this program.
 */
 
@@ -13,7 +13,7 @@ int main()
   DoubleList list1;  // List1 will be used for add and remove numbers
 	DoubleList list2;  // List2 will be used for copy from List1
 	cout<<"Adding ten elements to the list1. . . " <<endl;
-	
+
   list1.addDouble(10.25);
 	list1.addDouble(25.75);
 	list1.addDouble(30.25);
@@ -23,19 +23,22 @@ int main()
   list1.addDouble(50.75);
 	list1.addDouble(55.75);
 	list1.addDouble(60.25);
-  list1.addDouble(65.75);
+  list1.addDouble(25.75);
+  list1.addDouble(55.75);
+	list1.addDouble(80.25);
+  list1.addDouble(55.75);
 
   cout<<"done!" <<endl;
-  cout <<"********************************"<<endl;	 
+  cout <<"********************************"<<endl;
   cout<< "Here are the ten elements of the current list1" <<endl;
   for (int i=0; i<list1.getNumDoubles(); i++)
 		cout << list1.getDouble(i) << endl;
 	cout << "and the capacity of the array is: " <<list1.getArraySize() << " elements so far"<< endl;
-	cout <<"********************************"<<endl;	 
+	cout <<"********************************"<<endl;
   cout << "Here is the number of doubles actually in the list1: " <<list1.getNumDoubles() << " elements"<< endl;
 	cout << "and the capacity of the array is: " <<list1.getArraySize() << " elements so far"<< endl;
 
-  cout<<"********************************"<<endl;	 
+  cout<<"********************************"<<endl;
   cout << "Now adding adding five more elements to list1" << endl;
   list1.addDouble(80.25);
 	list1.addDouble(90.25);
@@ -44,16 +47,16 @@ int main()
 	list1.addDouble(90.25);
 
   cout <<"done!" <<endl;
-  cout <<"********************************"<<endl;	 
+  cout <<"********************************"<<endl;
   cout << "Here is the the current list1" <<endl;
   for (int j=0; j<list1.getNumDoubles(); j++)
 		cout<<list1.getDouble(j)<<endl;
-	
-	cout <<"********************************"<<endl;	 
+
+	cout <<"********************************"<<endl;
   cout << "Here is the number of doubles actually in the list: " << list1.getNumDoubles() << " elements"<< endl;
   cout << "and the capacity of the array is: " <<list1.getArraySize() << " elements so far"<< endl;
-	cout <<"********************************"<<endl;	
-  
+	cout <<"********************************"<<endl;
+
 	list1.removeDouble();
 	list1.removeDouble();
 	list1.removeDouble();
@@ -61,12 +64,12 @@ int main()
 	cout << "Here is the the current list after remove three elements from the list" <<endl;
   for (int j=0; j<list1.getNumDoubles(); j++)
 		cout << list1.getDouble(j)<< endl;
-	
-	cout <<"********************************"<<endl;	 
+
+	cout <<"********************************"<<endl;
   cout << "Here is the number of doubles actually in the list: " <<list1.getNumDoubles() << " elements"<< endl;
   cout << "and the capacity of the array is: " <<list1.getArraySize() << " elements so far"<< endl;
-	cout <<"********************************"<<endl;	
-			 
+	cout <<"********************************"<<endl;
+
   cout << "Assigning values from list1 to the list2 : " << endl;
   list2 = list1;
 
@@ -74,9 +77,7 @@ int main()
   for (int z=0; z<list2.getNumDoubles(); z++)
 		cout << list2.getDouble(z)<< endl;
 	cout << "and the capacity of the array of List2 is: " <<list2.getArraySize() << " elements so far"<< endl;
-	cout <<"********************************"<<endl;	 
-  
+	cout <<"********************************"<<endl;
+
   return 0;
 }
-
-
